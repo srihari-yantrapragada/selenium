064c1a23a68c79fe012960a4363bed6ed48e8f13
SRIHARI <SRIHARI@SRIHARI>
2018-07-19 22:50:42 +0530

    created Automated project

---
+++ AutomationProject/src/testcases/TC_001.java
@@ -0,0 +1,21 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_001 {
+
+	@Test
+	public void testcase1() {
+		
+		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("http://www.facebook.com");
+		driver.findElement(By.id("email")).sendKeys("srihari");
+		driver.findElement(By.id("pass")).sendKeys("srihari");
+		driver.quit();
+
+	}
+
+}
